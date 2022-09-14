20a3c3c756093423916150fbea410c41d0d8d16c
Flavio Ariano <fhariano@gmail.com>
2022-09-14 11:27:17 -0300

    ajustes em models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'cell_phone',
         'email',
         'password',
     ];
