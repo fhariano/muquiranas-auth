aebc134633274af68b6ec6bd671710e46e341996
Flavio Ariano <fhariano@gmail.com>
2022-11-15 19:49:36 -0300

    Ajustes SotreUpodateser

--- app/Http/Requests/StoreUpdateUser.php
+++ app/Http/Requests/StoreUpdateUser.php
@@ -31,12 +31,16 @@ class StoreUpdateUser extends FormRequest
             'password' => ['required', 'min:4', 'max:20'],
             'cpf' => ['required', 'min:11', 'max:11', "unique:users,cpf,{$uuid},uuid"],
             'cell_phone' => ['required', 'min:11', 'max:11', "unique:users,cell_phone,{$uuid},uuid"],
-            'password' => ['required', 'min:4', 'max:20'],
             'email' => ['required', 'email', 'max:255', "unique:users,email,{$uuid},uuid"],
         ];
 
         if($this->method() == 'PUT'){
-            $rules['password'] = ['nullable', 'min:4', 'max:20'];
+            $rules['full_home'] = ['nullable'];
+            $rules['short_name'] = ['nullable'];
+            $rules['password'] = ['nullable'];
+            $rules['cpf'] = ['nullable'];
+            $rules['cell_phone'] = ['nullable'];
+            $rules['email'] = ['nullable'];
         }
 
         return $rules;
