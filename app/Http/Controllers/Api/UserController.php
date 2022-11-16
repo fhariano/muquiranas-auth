4353898ec4ad54888991c33e275de390d9322151
Flavio Ariano <fhariano@gmail.com>
2022-11-16 11:37:25 -0300

    Ajustes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
     public function __construct(User $user)
     {
         $this->model = $user;
+        $this->middleware('can:users');
     }
 
     /**
